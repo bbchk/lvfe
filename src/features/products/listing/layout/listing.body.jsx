1d7a99362847426dbf53a6cd8b5f3353556a6cbf
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2025-10-21 13:03:09 +0300

    fix product page completel yeven more

--- src/features/products/listing/layout/listing.body.jsx
+++ src/features/products/listing/layout/listing.body.jsx
@@ -1,54 +1,45 @@
-import s from './listing.body.module.scss'
+import s from "./listing.body.module.scss";
 
-import { useMediaQuery, ThemeProvider } from '@mui/material'
-import { useSelector } from 'react-redux'
+import { useMediaQuery, ThemeProvider } from "@mui/material";
+import { useSelector } from "react-redux";
 
-import React from 'react'
+import React, { Suspense } from "react";
 
-import LoadingSpinner from 'comps/loading/spinner'
+import LoadingSpinner from "comps/loading/spinner";
 
-const NoProductYet = React.lazy(() =>() => import('comps/warnings/no_products'), {
-  loading: () => <LoadingSpinner />,
-  ssr: false,
-})
+const NoProductYet = React.lazy(() => import("comps/warnings/no_products"));
 
-const FiltersAccordion = React.lazy(() =>
+const FiltersAccordion = React.lazy(
   () =>
     import(
-      'features/products/listing/comps/filter/filters_accordion/filters_accordion'
+      "features/products/listing/comps/filter/filters_accordion/filters_accordion"
     ),
-  { loading: () => <LoadingSpinner /> },
-)
+);
 
-const FiltersOffcanvas = React.lazy(() =>
+const FiltersOffcanvas = React.lazy(
   () =>
     import(
-      'features/products/listing/comps/filter/filiters_offcanvas/filters_offcanvas'
+      "features/products/listing/comps/filter/filiters_offcanvas/filters_offcanvas"
     ),
-  { loading: () => <LoadingSpinner /> },
-)
+);
 
-const ProductsPagination = React.lazy(() =>
-  () =>
-    import('features/products/listing/comps/gallery/pagination/pagination'),
-  { ssr: false },
-)
+const ProductsPagination = React.lazy(
+  () => import("features/products/listing/comps/gallery/pagination/pagination"),
+);
 
-const Selected = React.lazy(() =>
-  () => import('features/products/listing/comps/filter/selected'),
-  { ssr: false },
-)
+const Selected = React.lazy(
+  () => import("features/products/listing/comps/filter/selected"),
+);
 
-const FiltersOffcanvasToggler = React.lazy(() =>
+const FiltersOffcanvasToggler = React.lazy(
   () =>
     import(
-      'features/products/listing/comps/filter/filiters_offcanvas/filters_offcanvas_toggler'
+      "features/products/listing/comps/filter/filiters_offcanvas/filters_offcanvas_toggler"
     ),
-  { ssr: true },
-)
+);
 
-import SortGroup from 'features/products/listing/comps/filter/sort-group'
-import ProductGallery from 'features/products/listing/comps/gallery/gallery'
+import SortGroup from "features/products/listing/comps/filter/sort-group";
+import ProductGallery from "features/products/listing/comps/gallery/gallery";
 
 const ProductListingBody = ({
   data: {
@@ -61,33 +52,50 @@ const ProductListingBody = ({
     page,
   },
 }) => {
-  const isSmallViewport = useMediaQuery('(max-width:1100px)') // Adjust the value as needed
-  //todo lazy load
-  const { filterOffcanvasOpen } = useSelector((state) => state.modals)
+  const isSmallViewport = useMediaQuery("(max-width:1100px)"); // Adjust the value as needed
+  const { filterOffcanvasOpen } = useSelector((state) => state.modals);
 
   return (
     <>
       {productsCount > 0 ? (
         <>
           {filterOffcanvasOpen && (
-            <FiltersOffcanvas
-              filters={filtersMap}
-              minMaxPrice={minMaxPrice}
-              productsCount={productsCount}
-            />
+            <Suspense fallback={<LoadingSpinner />}>
+              <FiltersOffcanvas
+                filters={filtersMap}
+                minMaxPrice={minMaxPrice}
+                productsCount={productsCount}
+              />
+            </Suspense>
           )}
 
           <div className={`${s.body}`}>
             {isSmallViewport && (
               <div className={`${s.filters_offcanvas_toggler}`}>
-                <FiltersOffcanvasToggler />
+                <Suspense fallback={<LoadingSpinner />}>
+                  <FiltersOffcanvasToggler />
+                </Suspense>
               </div>
             )}
             <div className={`${s.selected}`}>
+              <Suspense fallback={<LoadingSpinner />}>
+                <Selected productsCount={productsCount} />
+              </Suspense>
             </div>
             <div className={`${s.sort_group}`}>
               <SortGroup />
             </div>
+            <div className={`${s.filters_decor_line}`}></div>
+            {!isSmallViewport && (
+              <div className={`${s.filters}`}>
+                <Suspense fallback={<LoadingSpinner />}>
+                  <FiltersAccordion
+                    filters={filtersMap}
+                    minMaxPrice={minMaxPrice}
+                  />
+                </Suspense>
+              </div>
+            )}
 
             <div className={`${s.gallery}`}>
               <div className={`${s.products}`}>
@@ -96,16 +104,21 @@ const ProductListingBody = ({
                   activeCategory={category}
                 />
               </div>
+              <Suspense fallback={<LoadingSpinner />}>
+                <ProductsPagination numPages={numPages} activePageId={page} />
+              </Suspense>
             </div>
           </div>
         </>
       ) : (
         <div className={`${s.no_products}`}>
-          <NoProductYet />
+          <Suspense fallback={<LoadingSpinner />}>
+            <NoProductYet />
+          </Suspense>
         </div>
       )}
     </>
-  )
-}
+  );
+};
 
-export default ProductListingBody
+export default ProductListingBody;
